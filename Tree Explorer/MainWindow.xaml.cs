34708216dc288534c3c9f74d4f9c44dda0ab4cc9
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2022-03-03 02:46:51 +0700

    Input UI

---
+++ Tree Explorer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tree_Explorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog picker = new FolderBrowserDialog();
+            DialogResult result = picker.ShowDialog();
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                startingFolderName.Content = picker.SelectedPath;
+            }
+        }
+    }
+}
