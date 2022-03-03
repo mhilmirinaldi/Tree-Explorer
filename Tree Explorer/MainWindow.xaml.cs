2b825ab08eb12706d80dc98302fdf726f35a0035
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2022-03-03 17:44:11 +0700

    Graph visualization semi-finished
    Using MSAGL

--- Tree Explorer/MainWindow.xaml.cs
+++ Tree Explorer/MainWindow.xaml.cs
@@ -14,6 +14,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Msagl;
+using System.Drawing;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.WpfGraphControl;
+using System.IO;
 
 namespace Tree_Explorer
 {
@@ -22,9 +27,17 @@ namespace Tree_Explorer
     /// </summary>
     public partial class MainWindow : Window
     {
+        Tree dirTree;
+        Graph graph;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            graph = new Graph();
+            var edge = graph.AddEdge("A", "B");
+            //graph.Attr.LayerDirection = LayerDirection.LR;
+            graphControl.Graph = graph;
         }
 
         private void chooseFolderButton_Click(object sender, RoutedEventArgs e)
@@ -33,7 +46,58 @@ namespace Tree_Explorer
             DialogResult result = picker.ShowDialog();
             if(result == System.Windows.Forms.DialogResult.OK)
             {
-                startingFolderName.Content = picker.SelectedPath;
+                startingFolderName.Text = picker.SelectedPath;
+            }
+        }
+
+        void updateGraph(string path, Tree tree)
+        {
+            foreach(Tree node in tree.children)
+            {
+                Node graphNode = new Node(path + "\\" + node.info);
+                graphNode.LabelText = node.info;
+                this.graph.AddNode(graphNode);
+                this.graph.AddEdge(path, graphNode.Id);
+                if(node.children.Count > 0)
+                {
+                    updateGraph(path + "\\" + node.info, node);
+                }
+            }
+        }
+
+        // Memodifikasi atribut dirTree kelas ini
+        void constructDirectoryTree(string path)
+        {
+            FileInfo[]  fileInfos = new DirectoryInfo(path).GetFiles();
+            foreach(FileInfo fileInfo in fileInfos)
+            {
+                this.dirTree.children.Add(new Tree(fileInfo.Name));
+            }
+
+            DirectoryInfo[] directoryInfos = new DirectoryInfo(path).GetDirectories();
+            Tree originalTree = this.dirTree;
+            foreach (DirectoryInfo directoryInfo in directoryInfos)
+            {
+                Tree tree = new Tree(directoryInfo.Name, TreeColor.RED);
+                this.dirTree = tree;
+                constructDirectoryTree(directoryInfo.FullName);
+                originalTree.children.Add(tree);
+            }
+            this.dirTree = originalTree;
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string path = startingFolderName.Text;
+            if (!string.IsNullOrEmpty(path))
+            {
+                dirTree = new Tree(path, TreeColor.RED);
+                constructDirectoryTree(path);
+
+                this.graph = new Graph();
+                graph.AddNode(path);
+                updateGraph(path, this.dirTree);
+                graphControl.Graph = this.graph;
             }
         }
     }
---
+++ Tree Explorer/Tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_Explorer
+{
+    enum TreeColor
+    {
+        BLACK,
+        RED,
+        BLUE
+    }
+
+    internal class Tree
+    {
+        public string info; // nama file atau folder
+        public TreeColor nodeColor;
+        public List<Tree> children;
+
+        public Tree()
+        {
+            this.info = "";
+            this.nodeColor = TreeColor.BLACK;
+            this.children = new List<Tree>();
+        }
+
+        public Tree(string info)
+        {
+            this.info = info;
+            this.nodeColor = TreeColor.BLACK;
+            this.children = new List<Tree>();
+        }
+
+        public Tree(string info, TreeColor nodeColor)
+        {
+            this.info = info;
+            this.nodeColor = nodeColor;
+            this.children = new List<Tree>();
+        }
+    }
+}
