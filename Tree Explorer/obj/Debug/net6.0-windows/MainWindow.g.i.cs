7b04bd918e7424bf8c87ab90ebd10a9a553d33fc
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2022-03-23 01:37:23 +0700

    Add gitignore

--- Tree Explorer/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Tree Explorer/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -135,7 +135,7 @@ namespace Tree_Explorer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Tree Explorer;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Tree Explorer;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
